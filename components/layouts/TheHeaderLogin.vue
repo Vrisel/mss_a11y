2f31b389af8bfc15edf0d21a4c8a7e4b4d500bd3
Vrisel <vrisel.p@gmail.com>
2021-10-18 13:59:44 +0900

    Default layout 구조 잡기 및 TheHeader 1차 구현 - 갈 길이 멀긴 한데 일단 틀만 잡음

---
+++ components/layouts/TheHeaderLogin.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-col cols="auto">
+    <b-button>로그인</b-button>
+  </b-col>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="__default">
+    <TheHeader />
+    <TheAside />
+    <main>
+      <Nuxt />
+    </main>
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/layouts/TheHeader.vue';
+import TheAside from '@/components/layouts/TheAside.vue';
+import TheFooter from '@/components/layouts/TheFooter.vue';
+export default {
+  components: {
+    TheHeader,
+    TheAside,
+    TheFooter,
+  },
+};
+</script>
+
+<style>
+#__default {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+}
+</style>
