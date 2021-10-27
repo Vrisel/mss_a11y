daad50f24d71fb63da6dabd9cd1479195d5e7937
Vrisel <vrisel.p@gmail.com>
2021-10-27 17:53:31 +0900

    header 외형 수정
    - top banner 닫기 버튼 구현
    - Navbar 컴포넌트 분리

--- components/layouts/TheHeader.vue
+++ components/layouts/TheHeader.vue
@@ -1,106 +1,67 @@
 <template>
   <header role="banner">
-    <a v-if="topBanner" href="#">
-      <b-img
-        blank
-        blank-color="grey"
-        width="2600px"
-        height="70px"
-        alt="top banner"
-      />
-    </a>
+    <div v-if="topBanner">
+      <a href="#">
+        <b-img
+          blank
+          blank-color="grey"
+          width="2600px"
+          height="100px"
+          alt="top banner"
+        />
+      </a>
+      <b-button
+        type="button"
+        squared
+        size="sm"
+        variant="dark"
+        class="topbanner-close"
+        @click="closeTopBanner"
+      >
+        <b-icon icon="x" />
+      </b-button>
+    </div>
 
-    <b-navbar>
-      <b-navbar-brand to="/">MUSINSA</b-navbar-brand>
-      <b-nav-form>
-        <b-input-group size="sm">
-          <b-form-input type="search" name="q"></b-form-input>
-          <b-input-group-append>
-            <b-input-group-text>
-              <b-icon icon="camera" aria-hidden="true" />
-              <span class="sr-only">이미지 검색</span>
-            </b-input-group-text>
-            <b-button type="submit" size="sm">
-              <b-icon icon="search" aria-hidden="true" />
-              <span class="sr-only">검색</span>
-            </b-button>
-          </b-input-group-append>
-        </b-input-group>
-      </b-nav-form>
+    <TheHeaderNavbar class="bg-primary py-2" />
 
-      <!-- 랭킹 영역 -->
-
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item-dropdown no-caret>
-          <template #button-content>랭킹</template>
-          <b-dropdown-item>상품</b-dropdown-item>
-          <b-dropdown-item>브랜드</b-dropdown-item>
-          <b-dropdown-item>검색어</b-dropdown-item>
-        </b-nav-item-dropdown>
-        <b-nav-item-dropdown no-caret>
-          <template #button-content>업데이트</template>
-          <b-dropdown-item
-            >전체 신상품 세일 재입고 이벤트 입점 인기</b-dropdown-item
-          >
-        </b-nav-item-dropdown>
-        <b-nav-item to="/">브랜드</b-nav-item>
-        <b-nav-item-dropdown no-caret>
-          <template #button-content>스타일</template>
-          <b-dropdown-item>코디숍 코디맵 브랜드스냅 스트릿스냅</b-dropdown-item>
-        </b-nav-item-dropdown>
-        <b-nav-item-dropdown no-caret>
-          <template #button-content>스페셜</template>
-          <b-dropdown-item>쇼케이스 스페셜이슈 단독상품</b-dropdown-item>
-        </b-nav-item-dropdown>
-        <b-nav-item-dropdown no-caret>
-          <template #button-content>콘텐츠</template>
-          <b-dropdown-item>큐레이팅 비디오 회원후기</b-dropdown-item>
-        </b-nav-item-dropdown>
-        <b-nav-item-dropdown no-caret>
-          <template #button-content>세일</template>
-          <b-dropdown-item
-            >세일 타임세일 클리어런스 기획전 쿠폰</b-dropdown-item
-          >
-        </b-nav-item-dropdown>
-        <b-nav-item-dropdown no-caret>
-          <template #button-content>이벤트</template>
-          <b-dropdown-item>헐</b-dropdown-item>
-        </b-nav-item-dropdown>
-        <b-nav-item-dropdown no-caret>
-          <template #button-content>매거진</template>
-          <b-dropdown-item>뉴스 매거진</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-navbar>
-
-    <b-row align-h="start">
+    <b-row align-h="start" class="px-3 py-3 bg-light">
       <b-col cols="auto"><b-link>스탠다드</b-link></b-col>
       <b-col cols="auto"><b-link>테라스샵</b-link></b-col>
       <b-col cols="auto"><b-link>셀렉트</b-link></b-col>
     </b-row>
 
-    <b-row>
+    <b-row class="px-3 py-3">
       <TheHeaderLogin />
       <b-col cols="auto"><b-link>마이페이지</b-link></b-col>
-      <b-col cols="auto"
-        ><b-link
-          >장바구니 <b-badge pill variant="primary">0</b-badge></b-link
-        ></b-col
-      >
+      <b-col cols="auto">
+        <b-link>장바구니<b-badge pill variant="primary">0</b-badge></b-link>
+      </b-col>
     </b-row>
   </header>
 </template>
 
 <script>
+import TheHeaderNavbar from '@/components/layouts/TheHeaderNavbar.vue';
 import TheHeaderLogin from '@/components/layouts/TheHeaderLogin.vue';
 export default {
-  components: { TheHeaderLogin },
+  components: { TheHeaderNavbar, TheHeaderLogin },
   data() {
     return {
       topBanner: true,
     };
   },
+  methods: {
+    closeTopBanner() {
+      this.topBanner = false;
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.topbanner-close {
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+</style>
---
+++ components/layouts/TheHeaderNavbar.vue
@@ -0,0 +1,124 @@
+<template>
+  <b-navbar>
+    <b-navbar-brand to="/" class="h1 text-white">MUSINSA</b-navbar-brand>
+    <b-nav-form>
+      <b-input-group size="sm">
+        <b-form-input type="search" name="q"></b-form-input>
+        <b-input-group-append>
+          <b-input-group-text>
+            <b-icon icon="camera" aria-hidden="true" />
+            <span class="sr-only">이미지 검색</span>
+          </b-input-group-text>
+          <b-button type="submit" size="sm">
+            <b-icon icon="search" aria-hidden="true" />
+            <span class="sr-only">검색</span>
+          </b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </b-nav-form>
+
+    <!-- 랭킹 영역 -->
+
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item-dropdown no-caret>
+        <template #button-content>랭킹</template>
+        <b-dropdown-item v-for="n of nav.ranking" :key="n.name" :to="n.to">
+          {{ n.name }}
+        </b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item-dropdown no-caret>
+        <template #button-content>업데이트</template>
+        <b-dropdown-item v-for="n of nav.update" :key="n.name" :to="n.to">
+          {{ n.name }}
+        </b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item-dropdown no-caret>
+        <template #button-content>코디</template>
+        <b-dropdown-item v-for="n of nav.coordi" :key="n.name" :to="n.to">
+          {{ n.name }}
+        </b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item-dropdown no-caret>
+        <template #button-content>세일</template>
+        <b-dropdown-item v-for="n of nav.sale" :key="n.name" :to="n.to">
+          {{ n.name }}
+        </b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item-dropdown no-caret>
+        <template #button-content>스페셜</template>
+        <b-dropdown-item v-for="n of nav.special" :key="n.name" :to="n.to">
+          {{ n.name }}
+        </b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item-dropdown no-caret>
+        <template #button-content>이벤트</template>
+        <b-dropdown-item v-for="n of nav.event" :key="n.name" :to="n.to">
+          {{ n.name }}
+        </b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item to="/">브랜드</b-nav-item>
+      <b-nav-item-dropdown no-caret>
+        <template #button-content>매거진</template>
+        <b-dropdown-item v-for="n of nav.magazine" :key="n.name" :to="n.to">
+          {{ n.name }}
+        </b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nav: {
+        ranking: [
+          { name: '상품', to: '#' },
+          { name: '브랜드', to: '#' },
+          { name: '검색어', to: '#' },
+        ],
+        update: [
+          { name: '신상품', to: '#' },
+          { name: '재입고', to: '#' },
+          { name: '세일 소식', to: '#' },
+          { name: '이벤트', to: '#' },
+          { name: '입점', to: '#' },
+        ],
+        coordi: [
+          { name: '코디숍', to: '#' },
+          { name: '코디맵', to: '#' },
+          { name: '브랜드 스냅', to: '#' },
+          { name: '스트릿 스냅', to: '#' },
+        ],
+        sale: [
+          { name: '쿠폰', to: '#' },
+          { name: '세일', to: '#' },
+          { name: '타임세일', to: '#' },
+          { name: '클리어런스', to: '#' },
+          { name: '기획전', to: '#' },
+        ],
+        special: [
+          { name: '쇼케이스', to: '#' },
+          { name: '스페셜 이슈', to: '#' },
+          { name: '단독 상품', to: '#' },
+        ],
+        event: [
+          { name: '래플', to: '#' },
+          { name: '랜덤 이벤트', to: '#' },
+        ],
+        magazine: [
+          { name: '뉴스', to: '#' },
+          { name: '매거진', to: '#' },
+        ],
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+* {
+  font-size: 1.2rem;
+  font-weight: 600;
+}
+</style>
