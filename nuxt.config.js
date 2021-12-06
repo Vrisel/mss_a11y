fe4950410b63678d2dceb26232efce8463f1a3fa
Vrisel <vrisel.p@gmail.com>
2021-12-06 09:27:58 +0900

    vuex-persistedstate 패키지 관련 수정
    - static generate가 가능하도록 ssr: false 옵션 추가
    - 쿠키 만기 기간을 1일로 줄임

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,12 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/persistedState.js' }],
+  plugins: [
+    {
+      src: '~/plugins/persistedState.js',
+      ssr: false,
+    },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
