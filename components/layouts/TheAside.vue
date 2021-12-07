e6ba2ba5d830bb35683c6d52a3b385b536fab40d
Vrisel <vrisel.p@gmail.com>
2021-12-07 17:50:03 +0900

    aside 두번째 tabs 중 랭킹 탭의 브랜드 항목 구현
    - `vertical-align`은 테이블 구성요소나 inline 요소에만 적용되므로
    `<figure>` 안의 `<img>`를 수직정렬 시키고 싶다면
    두 요소가 모두 `display: inline-block;` 스타일을 가질 필요가 있음
      - 중앙정렬을 시키려면 역시 두 요소에 모두
      `vertical-align: middle;`을 부여해야 함....

--- components/layouts/TheAside.vue
+++ components/layouts/TheAside.vue
@@ -214,7 +214,12 @@
                       <strong class="position-absolute px-1 bg-white">
                         {{ i }}
                       </strong>
-                      <img src="https://via.placeholder.com/70x85" alt="" />
+                      <figure>
+                        <img
+                          src="https://via.placeholder.com/70x85"
+                          :alt="`${c} ${i}위 상품`"
+                        />
+                      </figure>
                       <p>
                         <span class="text-truncate">
                           {{ '브랜드명' }}
@@ -224,14 +229,14 @@
                           icon="triangle-fill"
                           scale="0.9"
                           variant="danger"
-                          aria-label=""
+                          aria-label="랭크 상승"
                         />
                         <b-icon
                           v-else-if="i % 3 === 2"
                           icon="dash"
                           scale="1.2"
                           variant="secondary"
-                          aria-label=""
+                          aria-label="랭크 유지"
                         />
                         <b-icon
                           v-else
@@ -239,7 +244,7 @@
                           flip-v
                           scale="0.9"
                           variant="primary"
-                          aria-label=""
+                          aria-label="랭크 하락"
                         />
                       </p>
                       <b-link class="stretched-link">{{ '상품명' }}</b-link>
@@ -252,7 +257,54 @@
               </b-tabs>
             </b-tab>
             <b-tab title="브랜드">
-              <div>흠</div>
+              <ul class="d-flex flex-wrap justify-content-between">
+                <li
+                  v-for="i of 9"
+                  :key="`brand-${i}`"
+                  class="position-relative hover-bg-light"
+                >
+                  <strong class="position-absolute px-1 bg-white">
+                    {{ i }}
+                  </strong>
+                  <figure
+                    class="d-inline-block align-middle"
+                    style="width: 70px; height: 70px; padding: 7.5px"
+                  >
+                    <img
+                      src="https://via.placeholder.com/55x20"
+                      class="w-100 d-inline-block align-middle"
+                      :alt="`${i}위 브랜드`"
+                    />
+                  </figure>
+                  <p>
+                    <b-link class="stretched-link text-truncate">
+                      {{ '브랜드명' }}
+                    </b-link>
+                    <b-icon
+                      v-if="i % 3 === 1"
+                      icon="triangle-fill"
+                      scale="0.9"
+                      variant="danger"
+                      aria-label="랭크 상승"
+                    />
+                    <b-icon
+                      v-else-if="i % 3 === 2"
+                      icon="dash"
+                      scale="1.2"
+                      variant="secondary"
+                      aria-label="랭크 유지"
+                    />
+                    <b-icon
+                      v-else
+                      icon="triangle-fill"
+                      flip-v
+                      scale="0.9"
+                      variant="primary"
+                      aria-label="랭크 하락"
+                    />
+                  </p>
+                </li>
+              </ul>
             </b-tab>
           </TheAsideSubtabs>
         </b-tab>
